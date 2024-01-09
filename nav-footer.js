bde4c866fd209c71a6dabc9701942ac5aa41cba1
nbrasshands <86249681+nbrasshands@users.noreply.github.com>
2024-01-09 11:54:08 -0500

    nav-footer.js

--- nav-footer.js
+++ nav-footer.js
@@ -1,38 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Set a threshold for desktop screens, for example, 1024px
-    var desktopThreshold = 768;
-
-    // Function to add or remove focus listeners
-    function updateFocusListeners(add) {
+    // Check if the screen width is greater than 768px
+    if (window.innerWidth > 768) {
         var inputs = document.querySelectorAll(".input-wrap input");
+
         inputs.forEach(function(input) {
-            if (add) {
-                input.addEventListener("focus", onFocus);
-                input.addEventListener("blur", onBlur);
-            } else {
-                input.removeEventListener("focus", onFocus);
-                input.removeEventListener("blur", onBlur);
-            }
+            input.addEventListener("focus", function() {
+                this.parentNode.classList.add("focused");
+            });
+
+            input.addEventListener("blur", function() {
+                this.parentNode.classList.remove("focused");
+            });
         });
     }
-
-    // Event handlers
-    function onFocus() {
-        this.parentNode.classList.add("focused");
-    }
-
-    function onBlur() {
-        this.parentNode.classList.remove("focused");
-    }
-
-    // Initial setup based on current screen width
-    updateFocusListeners(window.innerWidth >= desktopThreshold);
-
-    // Update listeners on window resize
-    window.addEventListener("resize", function() {
-        updateFocusListeners(window.innerWidth >= desktopThreshold);
-    });
 });
+
 
 
 document.addEventListener("DOMContentLoaded", function() {
